1831aaa283391fca34609e24d8b29c0264c7be2c
Luis Manuel Adames <luismanuelsuper@gmail.com>
2026-04-25 19:26:08 -0400

    Fix project structure (app/server/test separation)

--- app.js
+++ app.js
@@ -5,11 +5,4 @@ app.get("/", (req, res) => {
   res.send("Hola Mundo DevOps 🚀");
 });
 
-// IMPORTANTE: usar el puerto dinámico de Render
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
